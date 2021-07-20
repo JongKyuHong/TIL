767fdb04fc082f8c1c9f142f4216fd9f707ee54c
JongKyuHong <kongi7064@gmail.com>
2021-07-20 18:04:51 +0900

    18:04

---
+++ Algorithm/exponence/왕실의_나이트.py
@@ -0,0 +1,11 @@
+input_i = input()
+row = int(input_i[1])
+col = int(ord(input_i[0])) - int(ord('a')) + 1
+steps = [(-2,-1),(-1,-2),(1,-2),(2,-1),(2,1),(1,2),(-1,2),(-2,1)]
+result = 0
+for step in steps:
+    next_row = row + step[0]
+    next_col = col + step[1]
+    if next_row >= 1 and next_row <= 8 and next_col >= 1 and next_col <= 8:
+        result += 1
+print(result)
