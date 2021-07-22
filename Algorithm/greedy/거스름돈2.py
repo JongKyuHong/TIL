22d85734cf69d066048963a6341b2a70502099a3
JongKyuHong <kongi7064@gmail.com>
2021-07-22 22:02:57 +0900

    2021/07/22 22:02

---
+++ Algorithm/greedy/거스름돈2.py
@@ -0,0 +1,28 @@
+n = int(input())
+en = [500,100,50,10,5,1]
+def get_num(n):
+    n = 1000 -n
+    count = 0
+    while n>0:
+        if n >= en[0]:
+            n -= en[0]
+            count += 1
+        elif n >= en[1]:
+            n -= en[1]
+            count += 1
+        elif n >= en[2]:
+            n -= en[2]
+            count += 1
+        elif n >= en[3]:
+            n -= en[3]
+            count += 1
+        elif n >= en[4]:
+            n -= en[4]
+            count += 1
+        else:
+            n -= en[5]
+            count += 1
+    return count
+print(get_num(n))
+
+    
---
+++ Algorithm/greedy/로프.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+n = int(input())
+rope = []
+for _ in range(n):
+    rope.append(int(input()))
+print(rope)
+rope.sort()
+queue = deque(rope)
+target =  queue.popleft() * n
+maxa = target
+while n > 1:
+    n -= 1
+    target = queue.popleft() * n
+    if target > maxa:
+        maxa = target
+print(maxa)
