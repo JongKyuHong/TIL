c452a6ef9cf0a9ac19f7062664ee16b01c712640
JongKyuHong <kongi7064@gmail.com>
2021-07-28 15:26:56 +0900

    2021/07/28 15:26

---
+++ Algorithm/greedy/카드_합체_놀이.py
@@ -0,0 +1,15 @@
+import heapq
+
+n,m = map(int,input().split())
+state = list(map(int,input().split()))
+heapq.heapify(state)
+while m > 0:
+    a = heapq.heappop(state)
+    b = heapq.heappop(state)
+    heapq.heappush(state,a+b)
+    heapq.heappush(state,a+b)
+    m -= 1
+print(sum(state))
+
+
+
