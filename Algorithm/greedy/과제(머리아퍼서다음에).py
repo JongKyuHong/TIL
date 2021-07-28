241eea5ea40759334c4a36287383937aecfde758
JongKyuHong <kongi7064@gmail.com>
2021-07-28 18:09:24 +0900

    2021/07/28 18:09

---
+++ Algorithm/greedy/과제(머리아퍼서다음에).py
@@ -0,0 +1,10 @@
+import heapq
+
+n = int(input())
+
+array = []
+for _ in range(n):
+    array.append(list(map(int,input().split())))
+array.sort(key=lambda x:(x[0],x[1]),reverse=True)
+heapq.heapify(array)
+print(array)
