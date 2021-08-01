5420d96fc5a3dd04a384b857948ae4d04f3ea375
JongKyuHong <kongi7064@gmail.com>
2021-08-01 22:09:19 +0900

    2021/08/01 22:09

--- Algorithm/greedy/롤러코스터.py
+++ Algorithm/greedy/롤러코스터.py
@@ -1,82 +1,58 @@
-r,c = map(int,input().split())
+import sys
 
-array = []
-for _ in range(r):
-    array.append(list(map(int,input().split())))
+def printCodd():
+    for i in range(c//2):
+        print("D"*(r-1),end="")
+        print("R",end="")
+        print("U"*(r-1),end="")
+        print("R",end="")
+    print("D"*(r-1))
 
-# r이 짝수일때 분기가 나뉜다.
-# c가 홀수일때 세로로 볼수 있다. 즉 행의 갯수가 홀수일때 다 볼수있다 열도 마찬가지
-result = ''
+def printRoddCeven():
+    for i in range(r//2):
+        print("R"*(c-1),end="")
+        print("D",end="")
+        print("L"*(c-1),end="")
+        print("D",end="")
+    print("R"*(c-1))
 
-if r%2 and c%2: # 33이라고 생각해보자
+def printRevenCeven(temps):
+    min = 1000
+    indexi=-1
+    indexj=-1
     for i in range(r):
-        if i%2:
-            for _ in range(c-1):
-                result += 'L'  
-        else:
-            for _ in range(c-1):
-                result += 'R'
-        result +='D'
-    print(result[:len(result)-1])
-elif r%2==0 and c%2: # 행은 짝인데 열은 홀일때 즉 세로로 봐야한다.
-    for i in range(r):
-        if i%2:
-            for _ in range(c-1):
-                result += 'D'  
-        else:
-            for _ in range(c-1):
-                result += 'U'
-        result +='R'
-    print(result[:len(result)-1])
-elif r%2 and c%2==0: # 행은 홀이고 열은 짝일때 
-    for i in range(r):
-        if i%2:
-            for _ in range(c-1):
-                result += 'L'  
-        else:
-            for _ in range(c-1):
-                result += 'R'
-        result +='D'
-    print(result[:len(result)-1])
-else: #행이 짝이고 열도 짝일때
-    result1 = 0
-    result2 = 0
-    result3 = ''
-    result4 = ''
-    for i in range(r-1):
         for j in range(c):
-            result1 += array[i][j]
-            if i % 2 == 0:
-                result3 += 'R'
-            else:
-                result3 += 'L'
-        result3 = result3[:len(result3)-1]
-        result3 += 'D'
-    result1 += array[r-1][c-1]
-    result3 = result3[:len(result3)-1]
-    for i in range(r):
-        if i == r-2:
-            result2 += array[i][0]
-            result4 += 'D'
-            continue
-        for j in range(c):
-            result2 += array[i][j]
-            if i%2 == 0 and i>r-2:
-                result4 += 'L'
-            elif i%2 == 0:
-                result4 += 'R'
-            elif i%2 and i>r-2:
-                print('hi2')
-                result4 += 'R'
-            elif i%2:
-                result4 += 'L'
-        result4 = result4[:len(result4)-1]
-        result4 += 'D'
-    result4 = result4[:len(result4)-1]
-    if result1 > result2:
-        print(result1)
-        print(result3)
-    else:
-        print(result2)
-        print(result4)
+            if((i+j)%2!=0 and min>temps[i][j]):
+                min=temps[i][j]
+                indexi=i
+                indexj=j
+    res = ('D'*(r-1)+'R'+'U'*(r-1)+'R')*(indexj//2)
+    currentX=2*(indexj//2)
+    currentY=0
+    xbound=2*(indexj//2)+1
+    while currentX!=xbound or currentY!=r-1:
+        if(currentX<xbound and [currentY,xbound]!=[indexi,indexj]):
+            currentX+=1
+            res+='R'
+        elif currentX == xbound and [currentY, xbound - 1] != [indexi,indexj]:
+            currentX -= 1
+            res += 'L'
+        if currentY != r - 1:
+            currentY += 1
+            res += 'D'
+    res += ('R' + 'U' * (r - 1) + 'R' + 'D' * (r - 1)) * ((c - indexj - 1) // 2)
+    print(res)
 
+r, c = map(int, sys.stdin.readline().rstrip("\n").split())
+nums = []
+for i in range(r):
+    nums.append(list(map(int, sys.stdin.readline().rstrip().split())))
+start=nums[0][0]
+end=nums[r-1][c-1]
+temps=nums[:]
+if(c%2!=0):
+    printCodd()
+elif(r%2!=0):
+    printRoddCeven()
+elif(c%2==0 and r%2==0):
+    printRevenCeven(temps)
