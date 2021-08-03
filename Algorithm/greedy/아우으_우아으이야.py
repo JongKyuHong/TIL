7525b5e42c3ebf2ac30f5bd2c31b68f83a9dfede
JongKyuHong <kongi7064@gmail.com>
2021-08-04 01:33:53 +0900

    2021/08/04 01:33

---
+++ Algorithm/greedy/아우으_우아으이야.py
@@ -0,0 +1,16 @@
+n = int(input())
+x,y = map(int,input().split())
+
+result = 0
+
+for i in range(n-1):
+    xx, yy = map(int,input().split())
+    if xx == x:
+        y = yy
+    elif y>=xx and yy>y:
+        y = yy
+    elif y < xx:
+        result += abs(x-y)
+        x = xx
+        y = yy
+print(result + abs(y-x))
