21e6b4fe05cdcc6f585c85f9f9dcb54ba0bd1ca2
JongKyuHong <kongi7064@gmail.com>
2021-08-04 23:29:13 +0900

    2021/08/04 dir change 23:29

--- Algorithm/프린터큐.py
+++ Algorithm/프린터큐.py
@@ -14,17 +14,12 @@ for _ in range(t):
             cnt += 1
             break
         if maxq > quepop:
-            print(maxq ,"맥스큐")
-            print(que , "큐상황")
             que.append(quepop)     
             if idx == 0:
                 idx = len(que)
         else:
-            print(maxq,"맥스큐2")
-            print(que , "큐상황2")
             cnt += 1
             if idx == 0:
                 break
         idx -= 1
     print(cnt)         
-    
