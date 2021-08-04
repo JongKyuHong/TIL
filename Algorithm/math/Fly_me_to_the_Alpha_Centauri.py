b8b3981eb871380ff574cd14e4599aec7d65a83c
JongKyuHong <kongi7064@gmail.com>
2021-08-04 09:37:26 +0900

    2021/08/04 21:37

--- Algorithm/math/Fly_me_to_the_Alpha_Centauri.py
+++ Algorithm/math/Fly_me_to_the_Alpha_Centauri.py
@@ -1,34 +1,14 @@
 t = int(input())
 
 for _ in range(t):
-    start,end = map(int,input().split())
-    d = [0,1,2]
-    s = start + 1
-    e = end - 1
-    count = 0
-    d_sum = 0
-    while d_sum<((e-s)/2):
-        if d_sum + d[-1] < ((e-s)/2): # 1 <= 2, 3<=2
-            d_sum += d[-1] # 1
-            count += 1 # 1
-            d.append(d[-1]+1) # 2
-        elif d_sum+d[-1] == ((e-s)/2):
-            d_sum += d[-1]
-            count += 1
-            break
-        else:
-            for i in range(len(d)-1,len(d)-4,-1):
-                if d_sum + d[-i] < ((e-s)/2): # 1+1 <= 2
-                    d_sum += d[-i]
-                    count += 1
-                    if d[-i]+1 not in d:
-                        d.append(d[-1]+1)
-                elif d_sum + d[-i] == ((e-s)/2):
-                    d_sum += d[-i]
-                    count += 1 
-                    break
-    if e-s%2:
-        print(2*count+3)
-    else:
-        print(2*count+2)
-
+    x, y = map(int,input().split())
+    distance = y - x
+    count = 0  # 이동 횟수
+    move = 1  # count별 이동 가능한 거리
+    move_plus = 0  # 이동한 거리의 합
+    while move_plus < distance :
+        count += 1
+        move_plus += move  # count 수에 해당하는 move를 더함
+        if count % 2 == 0 :  # count가 2의 배수일 때, 
+            move += 1  
+    print(count)
