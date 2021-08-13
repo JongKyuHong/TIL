ad6e0786e368ec35f010374da9c346c7dd23fcb6
JongKyuHong <kongi7064@gmail.com>
2021-08-13 18:09:22 +0900

    2021/08/13 18:09

---
+++ Algorithm/biginner/그대로_출력하기.py
@@ -0,0 +1,12 @@
+import sys
+
+while 1:
+    a = sys.stdin.readline()
+    if a:
+        print(a,end='')
+    else:
+        break
+
+
+
+
---
+++ Algorithm/swea/String.py
@@ -0,0 +1,5 @@
+for _ in range(10):
+    t = int(input())
+    target = input()
+    text = input()
+    print(f'#{t} {text.count(target)}')
