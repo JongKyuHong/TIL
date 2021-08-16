05757da8afa941a2689d9b3ea628413135a8423a
JongKyuHong <kongi7064@gmail.com>
2021-08-16 14:39:45 +0900

    2021/08/16 14:39

---
+++ Algorithm/biginner/문자열분석.py
@@ -0,0 +1,15 @@
+while 1:
+    n = input().rstrip('\n')
+    if not n:
+        break
+    cnt1, cnt2, cnt3, cnt4 = 0, 0, 0, 0
+    for i in n:
+        if i.islower():
+            cnt1 += 1
+        elif i.isupper():
+            cnt2 += 1
+        elif i.isdigit():
+            cnt3 += 1
+        elif i.isspace():
+            cnt4 += 1
+    print(f'{cnt1} {cnt2} {cnt3} {cnt4}')
