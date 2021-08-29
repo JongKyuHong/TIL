824ca0068bb7966b5ca792fe7531483ded344242
JongKyuHong <kongi7064@gmail.com>
2021-08-29 19:25:07 +0900

    2021/08/29 19:25

--- Algorithm/im/개미.py
+++ Algorithm/im/개미.py
@@ -1,5 +1,23 @@
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input())
 
+w_list = []
+h_list = []
 
+for i in range(w):
+    w_list.append(i)
+for j in range(w, 0, -1):
+    w_list.append(i)
 
+for i in range(h):
+    h_list.append(i)
+for j in range(h, 0, -1):
+    h_list.append(i)
+
+x = w_list[(p+t) % (2*w)]
+y = h_list[(q+t) % (2*h)]
+
+print(x, y)
 
 
---
+++ Algorithm/im/전기버스.py
@@ -0,0 +1,25 @@
+for test_case in range(int(input())):
+    k, n, m = map(int, input().split())
+    array = list(map(int, input().split()))
+    if array[-1] != n:
+        array.append(n)
+    start, cnt, flag, a = 0, 0, 0, 0
+    while a < m+1:
+        if array[a] - start > k:
+            start = array[a-1]
+            if array[a] - start <= k:
+                cnt += 1
+            else:
+                cnt = 0
+                flag = 1
+                break
+        elif array[a] - start == k:
+            if a == m:
+                break
+            start = array[a]
+            cnt += 1
+        a += 1
+    print(f'#{test_case} {cnt}')
+
+
+
