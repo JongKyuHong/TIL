fe9b59def6b754d7753e3c833749c16562f1b9da
JongKyuHong <kongi7064@gmail.com>
2021-08-31 22:51:56 +0900

    2021/08/31 22:51

---
+++ Algorithm/im/연산자_끼워넣기.py
@@ -0,0 +1,29 @@
+n = int(input())
+array = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
+maximum = 1e-9
+minimum= 1e9
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == n:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+    
+    if plus:
+        dfs(depth+1, total + array[depth],plus -1,minus, multiply, divide)
+    if minus:
+        dfs(depth+1, total - array[depth],plus , minus-1 , multiply, divide)
+    if multiply:
+        dfs(depth+1, total * array[depth], plus, minus, multiply-1, divide)
+    if divide:
+        dfs(depth+1, int(total / array[depth]),plus,minus, multiply, divide-1)
+
+
+dfs(1, array[0], numbers[0], numbers[1], numbers[2], numbers[3])
+print(maximum)
+print(minimum)
+
+
+
+
