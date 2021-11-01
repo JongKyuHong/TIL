967aad498839757fd4311289db92a85b59076a5a
JongKyuHong <kongi7064@gmail.com>
2021-11-01 23:22:25 +0900

    2021/11/01

--- Algorithm/dp/퇴사.py
+++ Algorithm/dp/퇴사.py
@@ -1,11 +1,9 @@
 n = int(input())
-array = [ list(map(int, input().split())) for _ in range(n)]
+array = [list(map(int, input().split())) for _ in range(n)]
 dp = [0] * (n+1)
-max_d = 0
-for i in range(1,n+1):
-    dp[i] = max(dp[i-1], max_d)
-    if dp[i] > max_d:
-        max_d = dp[i]
 
-#https://jrc-park.tistory.com/119?category=378936
-
+for i in range(n-1, -1, -1):
+    if i + array[i][0] > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(array[i][1] + dp[i+array[i][0]], dp[i+1])
