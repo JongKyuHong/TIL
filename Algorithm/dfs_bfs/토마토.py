9d2dbc17be622515c69ba50b1120b893efd1a158
JongKyuHong <kongi7064@gmail.com>
2021-12-03 21:12:52 +0900

    토마토

--- Algorithm/dfs_bfs/토마토.py
+++ Algorithm/dfs_bfs/토마토.py
@@ -1,30 +1,37 @@
 from collections import deque
-delta = ((0,1),(0,-1),(1,0),(-1,0))
+delta = ((-1,0,0),(1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1))
 
-n, m = map(int, input().split())
-array = [list(map(int, input().split())) for _ in range(m)]
+m, n, h  = map(int, input().split())
+graph = []
 q = deque()
-for i in range(m):
+
+for i in range(h):
+    tmp = []
     for j in range(n):
-        if array[i][j] == 1:
-           q.append((i,j))
+        tmp.append(list(map(int, input().split())))
+        for k in range(m):
+            if tmp[j][k] == 1:
+                q.append([i,j,k])
+    graph.append(tmp)
 
 while q:
-    r, c = q.popleft()
-    for dr, dc in delta:
-        nr, nc = dr + r, dc + c
-        if nr < 0 or nc < 0 or nr >= m or nc >= n:
-            continue
-        if array[nr][nc] == 0:
-            array[nr][nc] = array[r][c] + 1
-            q.append((nr,nc))
-            
+    x, y, z = q.popleft()
+    for dx, dy, dz in delta:
+        nx = dx + x
+        ny = dy + y
+        nz = dz + z
+        if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m and graph[nx][ny][nz] == 0:
+            q.append([nx,ny,nz])
+            graph[nx][ny][nz] = graph[x][y][z] + 1
 
-ans = 0
-for i in array:
+day = 0
+for i in graph:
+    print(i)
     for j in i:
-        if j == 0:
-            print(-1)
-            exit()
-    ans = max(ans,max(i))
-print(ans-1)
+        for k in j:
+            if k == 0:
+                print(-1)
+                exit(0)
+        day = max(day, max(j))
+print(day-1)
+                
