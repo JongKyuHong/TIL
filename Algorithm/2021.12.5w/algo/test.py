428512f019b88e19159a751fab7d66dc132b510a
JongKyuHong <kongi7064@gmail.com>
2021-12-28 22:41:06 +0900

    2021/12/28

--- Algorithm/2021.12.5w/algo/test.py
+++ Algorithm/2021.12.5w/algo/test.py
@@ -1,11 +1,1 @@
-a = list(input())
-b = list(input())
-
-i = 0
-while i < len(a):
-    if a[i] in b:
-        b.remove(a[i])
-        a.remove(a[i])
-        i -= 1
-    i += 1
-print(len(a)+len(b))
+num = list(map(int, input().split()))
