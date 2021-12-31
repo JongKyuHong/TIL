97b4f16c919d1270b4d72dd4d6cbb30a32a7a6a0
JongKyuHong <kongi7064@gmail.com>
2021-12-31 16:24:34 +0900

    2021/12/30

--- Algorithm/2021.12.5w/algo/test.py
+++ Algorithm/2021.12.5w/algo/test.py
@@ -1,1 +1,11 @@
-num = list(map(int, input().split()))
+n = int(input())
+data = sorted(list(map(int, input().split())))
+result = 0
+
+for i in reversed(range(1, 1001)):
+    tmp1 = list(filter(lambda x: x >= i, data))
+    if len(tmp1) >= i:
+        result = i
+        break
+
+print(result)
