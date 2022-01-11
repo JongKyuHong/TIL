a2081684e8c4bcbd03d8ef622eece04b19325615
JongKyuHong <kongi7064@gmail.com>
2022-01-11 23:44:27 +0900

    solve

--- Algorithm/2022.01.3w/algo/test.py
+++ Algorithm/2022.01.3w/algo/test.py
@@ -1,42 +1,31 @@
-#치즈
+# 데스나이트
 from collections import deque
-
-m,n = map(int, input().split())
-
-graph = [list(map(int, input().split())) for _ in range(m)]
-delta = ((0,1),(0,-1),(1,0),(-1,0))
-def bfs():
+delta = ((-2,-1),(-2,1),(0,-2),(0,2),(2,-1),(2,1))
+def bfs(r1,c1):
     q = deque()
-    q.append((0,0))
-    visited = [[0]*n for _ in range(m)]
-    visited[0][0] = 1
+    maxdist = 0
+    q.append((r1,c1,0))
+    visited[r1][c1] = 1
     while q:
-        r,c = q.popleft()
-        for dr, dc in delta:
+        r, c, dist = q.popleft()
+        if r == r2 and c == c2:
+            maxdist = max(dist, maxdist)
+            return maxdist
+        for dr ,dc in delta:
             nr = dr + r
             nc = dc + c
-            if 0 <= nr < m and 0 <= nc < n:
-                if visited[nr][nc] == 0:
-                    if graph[nr][nc] >= 1:
-                        graph[nr][nc] += 1
-                    else:
-                        visited[nr][nc] = 1
-                        q.append((nr,nc))
-time = 0
-while 1:
-    bfs()
-    cnt = 0
-    for i in range(m):
-        for j in range(n):
-            if graph[i][j] >= 3:
-                graph[i][j] = 0
-                cnt += 1
-            elif graph[i][j] == 2:
-                graph[i][j] = 1
-    if cnt == 1:
-        time += 1
-    else:
-        break
-print(time)
-    
+            if 0 <= nr < n and 0 <= nc < n and not visited[nr][nc]:
+                visited[nr][nc] = 1
+                q.append((nr,nc,dist+1))
+              
+n = int(input())
+graph = [[0]*n for _ in range(n)]
+r1,c1,r2,c2 = map(int, input().split())
+visited = [[0]*n for _ in range(n)]
+res = bfs(r1,c1)
+if res:
+    print(res)
+else:
+    print(-1)
+
 
