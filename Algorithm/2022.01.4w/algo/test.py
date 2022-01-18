42b6a4e1de76538d1b63b665c11a11967209e20b
JongKyuHong <kongi7064@gmail.com>
2022-01-18 21:12:11 +0900

    플로이드워셜

--- Algorithm/2022.01.4w/algo/test.py
+++ Algorithm/2022.01.4w/algo/test.py
@@ -1,9 +1,28 @@
-#상근날드
+INF = int(1e9)
 
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-e = int(input())
+n = int(input())
+m = int(input())
 
-print(min(a,b,c)+min(d,e)-50)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+            
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+    
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print('INFINITY', end='')
+        else:
+            print(graph[a][b], end=' ')
+    print()
