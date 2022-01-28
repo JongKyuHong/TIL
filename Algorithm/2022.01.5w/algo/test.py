f843179e2ea38c2de57fa5da1647fb69230af741
JongKyuHong <kongi7064@gmail.com>
2022-01-28 22:30:45 +0900

    하루일과

--- Algorithm/2022.01.5w/algo/test.py
+++ Algorithm/2022.01.5w/algo/test.py
@@ -1,29 +1,13 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 
-v, e = map(int, input().split())
-indegree = [0] * (v+1)
+idx = 0
+result = -1
 
-graph = [[] for _ in range(v+1)]
-
-for _ in range(e):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    indegree[b] += 1
-
-def topology_sort():
-    result = []
-    q = deque()
-    for i in range(1, v+1):
-        if indegree[i] == 0:
-            q.append(i)
-    while q:
-        now = q.popleft()
-        result.append(now)
-        for i in graph[now]:
-            indegree[i] -= 1
-            if indegree[i] == 0:
-                q.append(i)
-    for i in result:
-        print(i, end=' ')
-        
-topology_sort()
+for i in range(5):
+    A = list(map(int, input().split()))
+    
+    if sum(A) > result:
+        result = sum(A)
+        idx = i+1
+print(idx, result)
