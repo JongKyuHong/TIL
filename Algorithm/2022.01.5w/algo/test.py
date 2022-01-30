49ecafc0a69afa54b261e96af24b0f07922fe41a
JongKyuHong <kongi7064@gmail.com>
2022-01-30 23:52:51 +0900

    하루일과

--- Algorithm/2022.01.5w/algo/test.py
+++ Algorithm/2022.01.5w/algo/test.py
@@ -1,7 +1,17 @@
-K, N, M = map(int, input().split())
-answer = (K*N)-M 
+import sys
+input = sys.stdin.readline
 
-if answer > 0:
-    print(answer)
-else:
-    print(0)
+a,b,c = map(int, input().split())
+
+if a==b==c:
+    print(10000+1000*a)
+elif a==b:
+    print(1000+100*a)
+elif b==c:
+    print(1000+100*b)
+elif a==c:
+    print(1000+100*a)
+elif a!=b and b!=c and a!=c:
+    m=max(a,b,c)
+    print(m*100)
+    
