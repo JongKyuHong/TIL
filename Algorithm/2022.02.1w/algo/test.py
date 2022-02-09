4bafbbf2cd38e02f92d8d9043ae9ebf3114896c4
JongKyuHong <kongi7064@gmail.com>
2022-02-09 23:35:34 +0900

    test

--- Algorithm/2022.02.1w/algo/test.py
+++ Algorithm/2022.02.1w/algo/test.py
@@ -1,9 +1,3 @@
-n, m = map(int, input().split())
-s = set(input() for _ in range(n))
-res = 0
-
-for _ in range(m):
-    word = input()
-    if word in s:
-        res += 1
-print(res)
+number = int(input())
+result = number - 543
+print(result)
