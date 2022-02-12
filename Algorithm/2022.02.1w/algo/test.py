b5f75132be146b04629e853d7d9634a48894f481
JongKyuHong <kongi7064@gmail.com>
2022-02-12 23:57:38 +0900

    일과

--- Algorithm/2022.02.1w/algo/test.py
+++ Algorithm/2022.02.1w/algo/test.py
@@ -1,4 +1,20 @@
-L, P = map(int, input().split())
-news = list(map(int, input().split()))
-for i in news:
-    print(i - L * P, end = ' ')
+n, m = map(int, input().split())
+
+answer = 0
+price_list = []
+
+for _ in range(m):
+    price = tuple(map(int, input().split()))
+    price_list.append(price)
+
+six_list = sorted(price_list, key=lambda x : x[0])
+one_list = sorted(price_list, key=lambda x : x[1])
+
+if six_list[0][0] <= one_list[0][1] * 6:
+    answer = six_list[0][0] * (n // 6) + one_list[0][1] * (n % 6)
+    if six_list[0][0] < one_list[0][1] * (n % 6):
+        answer = six_list[0][0] * (n//6 + 1)
+else:
+    answer = one_list[0][1] * n
+
+print(answer)
