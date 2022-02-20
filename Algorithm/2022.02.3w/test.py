13679479461d7a0641740cf89966a779c2d43458
JongKyuHong <kongi7064@gmail.com>
2022-02-20 22:32:17 +0900

    가장 가까운 조상

--- Algorithm/2022.02.3w/test.py
+++ Algorithm/2022.02.3w/test.py
@@ -1,11 +1,23 @@
-for _ in range(int(input())):
-    M, N, x, y = map(int, input().split())
-    f = 1
-    while(x <= M*N):
-        if x%N == y%N:
-            print(x)
-            f = 0
-            break
-        x += M
-    if f:
-        print(-1)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    parent = [0] * (n + 1)
+    for _ in range(n - 1):
+        i, j = map(int, input().split())
+        parent[j] = i
+    
+    a, b = map(int, input().split())
+    a_parents, b_parents = [0, a], [0, b] 
+    while parent[a]:
+        a_parents.append(parent[a])
+        a = parent[a]
+    while parent[b]:
+        b_parents.append(parent[b])
+        b = parent[b]
+        
+
+    i = 1
+    while a_parents[-i] == b_parents[-i]:
+        i += 1
+        
+    print(a_parents[-i + 1])
