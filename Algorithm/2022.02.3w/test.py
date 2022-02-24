b2fb1569e00e8a0275cd94604c0efa5a12b62324
JongKyuHong <kongi7064@gmail.com>
2022-02-24 23:38:16 +0900

    test

--- Algorithm/2022.02.3w/test.py
+++ Algorithm/2022.02.3w/test.py
@@ -1,16 +1,2 @@
-V = int(input())
-vote = list(str(input()))
-
-A = B = 0
-for v in vote:
-    if v == 'A':
-        A += 1
-    else:
-        B += 1
-
-if A > B:
-    print('A')
-elif A == B:
-    print('Tie')
-else:
-    print('B')
+a,b = map(int,input().split(" "))
+print(a*(b-1)+1)
