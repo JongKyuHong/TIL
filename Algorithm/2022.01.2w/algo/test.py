10e7100ee8cea79be03f8ddcd972425822009a5e
JongKyuHong <kongi7064@gmail.com>
2022-03-17 23:20:37 +0900

    algo

--- Algorithm/2022.01.2w/algo/test.py
+++
@@ -1,4 +0,0 @@
-for _ in range(int(input())):
-    n, string = input().split()
-    n = int(n)
-    print(string[:n-1], string[n:], sep='')
--- Algorithm/2022.02.4w/test.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-
-for _ in range(n):
-    print('* '* (n - n//2))
-    print(n)
-    print(' *'* (n//2))
