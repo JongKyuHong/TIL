594765d1149bba26d797e6f3d16504255ed6cda9
JongKyuHong <kongi7064@gmail.com>
2022-06-21 23:26:43 +0900

    feat: algorithm

--- Algorithm/2022/test.py
+++ Algorithm/2022/test.py
@@ -1,4 +1,18 @@
-for test_case in range(int(input())):
-    n = input()
-    print(n[0]+n[-1])
-
+for _ in range(int(input())):
+    array = list(input())
+    stack = []
+    for i in array:
+        if i == '(':
+            stack.append(i)
+        else:
+            if stack:
+                if stack[-1] == '(':
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+    if stack:
+        print('NO')
+    else:
+        print('YES')
