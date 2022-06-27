77e92667b2f34dfab9f96591bb5f22a2f810ab4b
JongKyuHong <kongi7064@gmail.com>
2022-06-27 18:18:07 +0900

    feat: 2*n타일링

--- Algorithm/2022/test.py
+++ Algorithm/2022/test.py
@@ -1,17 +1,9 @@
 n = int(input())
-m = int(input())
-array = list(map(int, input().split()))
-array.sort()
-cnt = 0
+dp = [0]*1001
+dp[1] = 1
+dp[2] = 2
 
-for i in range(n-1):
-    ans = array[i]
-    for j in range(i+1, n):
-        if ans + array[j] == m:
-            cnt += 1
-            break
-        elif ans + array[j] > m:
-            break
-        else:
-            ans += array[j]
-print(cnt)
+if n > 2:
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+print(dp[n])
