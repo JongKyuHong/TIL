1801e7047ebe5c2ff340579291c41548cd252d45
HongJongKyu <kongi7064@gmail.com>
2022-08-22 18:22:21 +0900

    til

---
+++ Algorithm/2022/나무자르기.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
+trees.sort()
+left, right = trees[0], trees[-1]
+max_v, target = 0, 0
+
+while left < right:
+    mid = (left+right)//2
+    val = 0
+    for tree in trees:
+        if mid < tree:
+            val += tree-mid
+    if val >= m:
+        if target < mid:
+            target = mid
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(target)
