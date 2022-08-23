9d06e27c3a93dde3498f184047ac870d1cf5d9ee
HongJongKyu <kongi7064@gmail.com>
2022-08-23 13:11:04 +0900

    til

---
+++ Algorithm/2022/트리의부모찾기.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+q = deque([])
+q.append(1)
+
+visited = [0] * (n+1)
+
+def bfs():
+    while q:
+        now = q.popleft()
+        for next_node in graph[now]:
+            if visited[next_node] == 0:
+                visited[next_node] = now
+                q.append(next_node)
+
+
+bfs()
+for i in range(2,n+1):
+    print(visited[i])
