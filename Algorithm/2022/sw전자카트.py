d635daf3be40bda35126d2a3d1f9f78d8fb8d417
HongJongKyu <kongi7064@gmail.com>
2022-08-29 20:49:55 +0900

    til

---
+++ Algorithm/2022/sw전자카트.py
@@ -0,0 +1,27 @@
+def find(r, sum_value):
+    global min_value
+    for i in range(1, n):
+        if not visited[i]:
+            break
+    else:
+        sum_value += graph[r][0]
+        min_value = min(min_value, sum_value)
+        return min_value
+
+    for i in range(1, n):
+        if i != r and not visited[i]:
+            visited[i] = 1
+            find(i, sum_value+graph[r][i])
+            visited[i] = 0
+
+for T in range(int(input())):
+    n = int(input())
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    min_value = 1001
+    visited = [0] * n
+    sum_value = 0
+    find(0, sum_value)
+    
+    
+
+    print(f'#{T+1} {min_value}')
---
+++ Algorithm/2022/sw컨테이너운반.py
@@ -0,0 +1,18 @@
+for T in range(int(input())):
+    n, m = map(int, input().split()) # 화물 수, 트럭수
+    w = list(map(int, input().split())) # 화물무게
+    t = list(map(int, input().split())) # 트럭 적재용량
+    res_t = []
+    w.sort()
+    t.sort()
+    idx = 0
+    visited = [0]*n
+    res = 0
+    for i in range(m-1,-1,-1):
+        for j in range(n-1,-1,-1):
+            if t[i] >= w[j] and not visited[j]:
+                visited[j] = 1
+                res += w[j]
+                break
+    print(f'#{T+1} {res}')     
+
