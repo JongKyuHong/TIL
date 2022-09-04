083d5b5ce30a40cd90dda5210b1f195ae5c57b1b
HongJongKyu <kongi7064@gmail.com>
2022-09-04 15:30:37 +0900

    til

--- Algorithm/2022/나는친구가적다small.py
+++ Algorithm/2022/나는친구가적다small.py
@@ -1,20 +1,17 @@
 S = input()
-s = ''
-for i in S:
-    if i.isalpha():
-        s += i
 
 k = input()
 idx, flag = 0, 0
-for i in s:
-    target = k[idx]
-    if i == target:
-        idx += 1
-        if idx == len(s):
-            flag = 1
-            break
-    else:
-        idx = 0
+for i in S:
+    if i.isalpha():
+        target = k[idx]
+        if i == target:
+            idx += 1
+            if idx == len(k):
+                flag = 1
+                break
+        else:
+            idx = 0
 
 if flag:
     print(1)
---
+++ Algorithm/2022/염색체.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+a = input()
+b = input()
+if b in a:
+    print(1)
+else:
+    print(0)
+    
