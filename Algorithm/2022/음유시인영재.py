2035eb60bd589f5a3b515e11da7adccc990e9806
HongJongKyu <kongi7064@gmail.com>
2022-09-06 02:04:07 +0900

    til

---
+++ Algorithm/2022/음유시인영재.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+S = input().rstrip()
+ans = ''.join(a[0].upper() for a in S.split())
+S += ans
+space = int(input())
+alpha = list(map(int, input().split()))
+prev = ''
+for i in S:
+    if prev == i:
+        continue
+    if i == ' ':
+        space -= 1
+        if space < 0:
+            print(-1)
+            exit()
+    else:
+        target = ord(i.lower())-97
+        print(i, target,'tt')
+        if alpha[target] == 0:
+            print(-1)
+            exit()
+        alpha[target] -= 1
+        prev = i
+print(ans)
