c288b1902bac2de35b35d0214f6eb96cfe8f09dc
HongJongKyu <kongi7064@gmail.com>
2022-10-10 11:30:41 +0900

    til

---
+++ Algorithm/2022/주사위게임.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a, b = 100, 100
+for _ in range(n):
+    x, y = map(int, input().split())
+    if x > y:
+        b -= x
+    elif x < y:
+        a -= y
+print(a, b)
