a85a881f12071e5d51fa18992c70a84da57d7eed
HongJongKyu <kongi7064@gmail.com>
2022-10-16 19:35:51 +0900

    til

---
+++ Algorithm/2022/3의배수.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+X = list(input().rstrip())
+convert = 0
+if len(X) == 1:
+    print(0)
+    if int(X[0]) % 3 == 0:
+        print('YES')
+    else:
+        print('NO')
+while 1:
+    sum_v = 0
+    for i in X:
+        sum_v += int(i)
+    convert += 1
+    if sum_v < 10:
+        print(convert)
+        if sum_v in [3,6,9]:
+            print('YES')
+        else:
+            print('NO')
+        break
+    X = list(str(sum_v))
