604859d3119671a8fb4e2fb3d98e002c22346da1
HongJongKyu <kongi7064@gmail.com>
2022-11-04 17:58:20 +0900

    til

--- Algorithm/2022/도시분할계획.py
+++ Algorithm/2022/도시분할계획.py
@@ -1,35 +1,37 @@
+import sys
+input = sys.stdin.readline
+
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
-
-def union_parent(parent, a, b):
+    
+def union_parent(a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
+    if a > b:
         parent[a] = b
-        
+    else:
+        parent[b] = a
+
+
 n, m = map(int, input().split())
 parent = [0] * (n+1)
-graph = []
-
-for i in range(n+1):
+edges = []
+result = 0
+for i in range(1, n+1):
     parent[i] = i
-    
+
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph.append((c,a,b))
-graph.sort()
-last = 0
-res = 0
+    edges.append((c,a,b))
 
-for edge in graph:
+edges.sort()
+for edge in edges:
     cost, a, b = edge
     if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        res += cost
+        union_parent(a, b)
+        result += cost
         last = cost
-print(res - last)
-    
+
+print(result-last)
