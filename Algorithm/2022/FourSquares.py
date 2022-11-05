b2fa69455b0f0f8975dfe31c3381c415c70f70ce
HongJongKyu <kongi7064@gmail.com>
2022-11-05 11:07:41 +0900

    til

--- Algorithm/2022/FourSquares.py
+++ Algorithm/2022/FourSquares.py
@@ -4,16 +4,11 @@ input = sys.stdin.readline
 
 #모든 자연수는 넷 혹은 그 이하의 제곱수의 합으로 표현가능
 n = int(input())
-dp = [50001 for i in range(n+1)]
+dp = [float('inf')]*(n+1)
 dp[0] = 0
 for i in range(1, n+1):
-    min_v = sys.maxsize
     j = 1
-
     while (j**2) <= i:
-        min_v = min(min_v, dp[i-(j**2)])
+        dp[i] = min(dp[i], dp[i-(j**2)]+1)
         j += 1
-    dp.append(min_v+1)
 print(dp[n])
-
-# 자신의 수에서 그보다 작은 수의 제곱수를 뺀것의 최소
