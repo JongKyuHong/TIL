93c953801110e5c48a74a2aec597c3f20369b9fd
HongJongKyu <kongi7064@gmail.com>
2022-11-23 22:36:09 +0900

    til

---
+++ Algorithm/2022/돌려돌려돌림판.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    X = input().rstrip().split()
+    x = ''
+    for i in X:
+        x += i
+    x = int(x)
+
+    Y = input().rstrip().split()
+    y = ''
+    for i in Y:
+        y += i
+    y = int(y)
+    state = list(map(int, input().split()))
+    state += state[:m-1]
+    idx = 0
+    ans = 0
+    while idx <= len(state)-m:
+        target = ''
+        print(state[idx:idx+m])
+        for i in state[idx:idx+m]:
+            target += str(i)
+        if x <= int(target) <= y:
+            ans += 1
+        idx += 1
+    print(ans)
