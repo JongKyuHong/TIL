273ed45be70572ed59d39db887b47be32238a672
HongJongKyu <kongi7064@gmail.com>
2022-11-30 16:48:38 +0900

    til

--- Algorithm/2022/주사위굴리기.py
+++ Algorithm/2022/주사위굴리기.py
@@ -1,43 +1,58 @@
+import sys
+input = sys.stdin.readline
+
 n, m, x, y, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+moves = list(map(int, input().split()))
+dice = [0,0,0,0,0,0]
 
-board = []
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-dice = [0, 0, 0, 0, 0, 0]
-
-def turn(dir):
-    a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
-    if dir == 1: #동
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
-
-    elif dir == 2: #서
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
-
-    elif dir == 3: #북
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
-
+def dir(i, j, move):
+    global x, y
+    a,b,c,d,e,f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] # 위, 앞, 왼, 뒤, 오, 아래
+    if move == 1 and j+1 < m:
+        j += 1
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, d, a, e
+        if graph[i][j]:
+            dice[5] = graph[i][j]
+            graph[i][j] = 0
+        else:
+            graph[i][j] = dice[5]
+        y = j
+    elif move == 2 and j-1 >= 0:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, b, a, d, f, c
+        j -= 1
+        if graph[i][j]:
+            dice[5] = graph[i][j]
+            graph[i][j] = 0
+        else:
+            graph[i][j] = dice[5]
+        y = j
+    elif move == 3 and i-1 >= 0:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, a ,c, f, e, b
+        i -= 1
+        if graph[i][j]:
+            dice[5] = graph[i][j]
+            graph[i][j] = 0
+        else:
+            graph[i][j] = dice[5]
+        x = i
+    elif move == 4 and i+1 < n:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, a, e, d
+        i += 1
+        if graph[i][j]:
+            dice[5] = graph[i][j]
+            graph[i][j] = 0
+        else:
+            graph[i][j] = dice[5]
+        x = i
     else:
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
+        return 0
 
-for i in range(n):
-    board.append(list(map(int, input().split())))
+    return dice
 
-comm = list(map(int, input().split()))
-
-nx, ny = x, y
-for i in comm:
-    nx += dx[i-1]
-    ny += dy[i-1]
-
-    if nx < 0 or nx >= n or ny < 0 or ny >= m:
-        nx -= dx[i-1]
-        ny -= dy[i-1]
-        continue
-    turn(i)
-    if board[nx][ny] == 0:
-        board[nx][ny] = dice[-1]
-    else:
-        dice[-1] = board[nx][ny]
-        board[nx][ny] = 0
-
-    print(dice[0])
+for move in moves:
+    aa = dir(x, y, move)
+    #print(aa, x, y, 'aa')
+    if aa:
+        dice = aa[:]
+        print(dice[0])
