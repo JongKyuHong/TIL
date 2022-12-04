b57002db68138ca25c75589caaf9bde46399220d
HongJongKyu <kongi7064@gmail.com>
2022-12-04 13:13:54 +0900

    til

--- Algorithm/2022/최소스패닝트리.py
+++ Algorithm/2022/최소스패닝트리.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -6,21 +9,22 @@ def find_parent(parent, x):
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
+
 v, e = map(int, input().split())
 edges = []
-parent = [0] * (v+1)
+parent = [0]*(v+1)
 
 for i in range(1, v+1):
     parent[i] = i
+
 for _ in range(e):
-    a, b, value = map(int, input().split())
-    edges.append((value, a, b))
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
 
 edges.sort()
 res = 0
