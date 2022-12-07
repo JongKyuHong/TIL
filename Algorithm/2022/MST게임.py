89c4ef254c3cf9d0ec9755d935f8650b5df26c20
HongJongKyu <kongi7064@gmail.com>
2022-12-08 01:55:03 +0900

    til

--- Algorithm/2022/MST게임.py
+++ Algorithm/2022/MST게임.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-def find_parent(x):
+def find(x):
     if parent[x] != x:
-        parent[x] = find_parent(parent[x])
+        parent[x] = find(parent[x])
     return parent[x]
 
-def union_parent(a, b):
-    a = find_parent(a)
-    b = find_parent(b)
+def union(a, b):
+    a = find(a)
+    b = find(b)
     if a > b:
         parent[a] = b
     else:
@@ -16,23 +16,28 @@ def union_parent(a, b):
 
 n, m, k = map(int, input().split())
 edges = []
-
 for i in range(1,m+1):
-    a, b = map(int, input().split())
-    edges.append((i,a,b))
+    x, y = map(int, input().split())
+    edges.append((i, x, y))
+edges.sort()
 
+flag = 0
 for i in range(k):
-    parent = [i for i in range(n+1)]
-    res = 0
-    cnt = 0
-    for cost, a, b in edges[i:]:
-        if find_parent(a) != find_parent(b):
-            union_parent(a, b)
-            res += cost
-            cnt += 1
-        if cnt == n-1:
-            print(res, end=' ')
-            break
-    else:
+    if flag:
         print(0, end=' ')
-
+    else:
+        parent = [i for i in range(n+1)]
+        cnt = 0
+        res = 0
+        for edge in edges[i:]:
+            cost, a, b = edge
+            if find(a) != find(b):
+                union(a, b)
+                res += cost
+                cnt += 1
+            if cnt == n-1:
+                print(res, end=' ')
+                break
+        else:
+            print(0, end=' ')
+            flag = 1
