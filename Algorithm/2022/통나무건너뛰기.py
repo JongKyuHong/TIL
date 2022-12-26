b174a99518e900ee56b2b249c27e59c5c0e4b762
HongJongKyu <kongi7064@gmail.com>
2022-12-26 15:41:53 +0900

    til

--- Algorithm/2022/통나무건너뛰기.py
+++ Algorithm/2022/통나무건너뛰기.py
@@ -1,6 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
     n = int(input())
-    li = list(map(int, input().split()))
-    li.sort()
-    li = li + li[0]
-    print(li)
+    lst = list(map(int, input().split()))
+    lst.sort(reverse=True)
+    tmp = [lst.pop()]
+    while lst:
+        tmp = [lst.pop()] + tmp
+        if lst:
+            tmp = tmp + [lst.pop()]
+    max_v = max(0, abs(tmp[0]-tmp[n-1]))
+    for i in range(n-1):
+        max_v = max(max_v, abs(tmp[i]-tmp[i+1]))
+    print(max_v)
