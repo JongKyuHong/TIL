ef307f2892e6947198283f553b49e5e243b7d62d
HongJongKyu <kongi7064@gmail.com>
2022-12-27 23:04:17 +0900

    til

--- Algorithm/2022/최소비용구하기2.py
+++ Algorithm/2022/최소비용구하기2.py
@@ -4,27 +4,31 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-graph = [[] for _ in range(n+1)]
+graph = [[]*(n+1) for _ in range(n+1)]
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b,c))
 start, end = map(int, input().split())
 
-q = [[0, start, [start]]]
-dist = [sys.maxsize] * (n+1)
-dist_path = [[] for _ in range(n+1)]
-dist[start] = 0
-while q:
-    now_dist, now, path = heapq.heappop(q)
-    if dist[now] < now_dist:
-        continue
-    for next_node, next_dist in graph[now]:
-        d = next_dist + now_dist
-        if dist[next_node] > d:
-            dist[next_node] = d
-            dist_path[next_node] = path + [next_node]
-            heapq.heappush(q,(d, next_node, dist_path[next_node]))
-
+def dijkstra(start):
+    path = [start]
+    q = [[0, start, path]]
+    dist[start] = 0
+    while q:
+        cost, now, path = heapq.heappop(q)
+        if now == end:
+            return path
+        if dist[now] < cost:
+            continue
+        for next_node, next_dist in graph[now]:
+            d = next_dist + cost
+            if dist[next_node] > d:
+                dist[next_node] = d
+                heapq.heappush(q, (d, next_node, path+[next_node]))
+            
+INF = float('inf')
+dist = [INF]*(n+1)
+path = dijkstra(start)
 print(dist[end])
-print(len(dist_path[end]))
-print(*dist_path[end])
+print(len(path))
+print(*path)
