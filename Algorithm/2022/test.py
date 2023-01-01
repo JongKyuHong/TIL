ab8545278bdfef81f344fe3a4cc7a906a50c1d7d
HongJongKyu <kongi7064@gmail.com>
2023-01-01 10:45:28 +0900

    til

--- Algorithm/2022/test.py
+++ Algorithm/2022/test.py
@@ -1,12 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-x, y = map(int, input().split())
+words = input().rstrip()
 
-def gcd(x, y):
-    if x > y:
-        x, y = y, x
-    while y:
-        x, y = y, x%y
-    return x
-print(x+y-gcd(x,y))
+
