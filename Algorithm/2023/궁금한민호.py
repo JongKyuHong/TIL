34400b7f91a73f1fb25d812b82cff2aabc55fc3c
HongJongKyu <kongi7064@gmail.com>
2023-02-01 18:54:48 +0900

    til

--- Algorithm/2023/궁금한민호.py
+++ Algorithm/2023/궁금한민호.py
@@ -6,8 +6,10 @@ graph = [list(map(int, input().split())) for _ in range(N)]
 dist = [[1]*N for _ in range(N)]
 for k in range(N):
     for i in range(N):
+        if i == k:
+            continue
         for j in range(N):
-            if i == j or j == k or i == k:
+            if i == j or j == k:
                 continue
             if graph[i][j] > graph[i][k] + graph[k][j]:
                 print(-1)
@@ -16,7 +18,7 @@ for k in range(N):
                 dist[i][j] = 0
 ans = 0
 for i in range(N):
-    for j in range(i, N):d
+    for j in range(i, N):
         if dist[i][j]:
             ans += graph[i][j]
 print(ans)
