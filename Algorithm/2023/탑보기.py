a1465fd13a551b263b29f27796db1cfb463d327f
HongJongKyu <kongi7064@gmail.com>
2023-02-12 18:32:30 +0900

    til

--- Algorithm/2023/탑보기.py
+++ Algorithm/2023/탑보기.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 N = int(input())
 lst = list(map(int, input().split()))
+near = [[float('inf'),float('inf')] for _ in range(N+1)]
 stack = []
-near = [[float('inf'),float('inf')] for _ in range(N)]
-cnt = [0]*N
+cnt = [0]*(N+1)
 for i in range(N):
     while stack and stack[-1][1] <= lst[i]:
         stack.pop()
@@ -20,9 +20,8 @@ for i in range(N):
             near[i][0] = stack[-1][0]
     stack.append([i, lst[i]])
 
-
 stack = []
-for i in range(N-1,-1,-1):
+for i in range(N-1, -1, -1):
     while stack and stack[-1][1] <= lst[i]:
         stack.pop()
     cnt[i] += len(stack)
@@ -35,9 +34,10 @@ for i in range(N-1,-1,-1):
         elif g == near[i][1] and stack[-1][0] < near[i][0]:
             near[i][0] = stack[-1][0]
     stack.append([i, lst[i]])
-        
+
 for i in range(N):
     if cnt[i] > 0:
         print(str(cnt[i]), str(near[i][0]+1))
     else:
         print(0)
+
