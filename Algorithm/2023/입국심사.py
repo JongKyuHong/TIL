ac0e19053fcc5a7973b36d69a0441ecbcbbbc350
HongJongKyu <kongi7064@gmail.com>
2023-03-27 14:23:16 +0900

    til

---
+++ Algorithm/2023/입국심사.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+time = [int(input()) for _ in range(N)]
+start,end = min(time), max(time)*M
+ans = end
+while start <= end:
+    mid = (start+end)//2
+    total = 0
+    for i in range(N):
+        total += mid//time[i]
+    if total >= M:
+        end = mid - 1
+        ans = min(ans, mid)
+    else:
+        start = mid + 1
+print(ans)
+        
