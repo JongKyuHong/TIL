0ee2256e06c6207b5a3324296ce8bc2cfcea4b74
HongJongKyu <kongi7064@gmail.com>
2023-04-25 14:22:00 +0900

    til

--- Algorithm/2023/세개의소수문제.py
+++ Algorithm/2023/세개의소수문제.py
@@ -1,35 +1,19 @@
-def printSum(pL, s):
-    for n1 in pL:
-        for n2 in pL:
-            for n3 in pL:
-                if n1 + n2 + n3 == s:
-                    print(n1, n2, n3)
-                    return
-    print(0)
- 
- 
-def findPrime(num):
-    data = list(range(2, num + 1))
-    result = []
- 
-    while len(data) != 0:
-        m = data[0]
-        result.append(m)
-        cnt = 0
-        while cnt < len(data):
-            if data[cnt] % m == 0:
-                data.pop(cnt)
-                cnt -= 1
-            cnt += 1
- 
-    return result
- 
- 
-T = int(input())
-N = []
- 
-for i in range(T):
-    N.append(int(input()))
- 
-for n in N:
-    printSum(findPrime(n), n)
+def find(num):
+    for i in range(len(primes)):
+        for j in range(i, len(primes)):
+            for k in range(j, len(primes)):
+                if primes[i] + primes[j] + primes[k] == num:
+                    return [primes[i], primes[j], primes[k]]
+
+nums = [0, 0] + [1]*(999)
+primes = []
+
+for i in range(2, 1001):
+    if nums[i]:
+        primes.append(i)
+        for j in range(2*i, 1001, i):
+            nums[j] = 0
+
+for _ in range(int(input())):
+    K = int(input())
+    print(*find(K))
