1a83f355c083a1c901dd8ed937896d28b32f0084
HongJongKyu <kongi7064@gmail.com>
2023-05-13 11:30:09 +0900

    til

---
+++ Algorithm/2023/줄세우기10431.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+P = int(input())  
+arr = [list(map(int, input().split())) for _ in range(P)]  
+height = [[] for _ in range(P)]
+result = [[] for _ in range(P)]
+cnt = 0
+
+for i in range(P):
+    cnt=0
+    height[i].append(arr[i][0])
+    height[i].append(arr[i][1])
+    for j in range(2, 21):  
+        for k in range(1, len(height[i])):  
+            if (arr[i][j] < height[i][k]): 
+                height[i].insert(k, arr[i][j]) 
+                cnt += len(height[i]) - 1 - k
+                break
+            if k == len(height[i]) - 1: 
+                height[i].append(arr[i][j]) 
+    result[i].append(arr[i][0]) 
+    result[i].append(cnt)   
+
+for i in range(P):
+    print(result[i][0], result[i][1])
