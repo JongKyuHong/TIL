4b9bf452957ccd4c5d1c794dc76a260677943acc
HongJongKyu <kongi7064@gmail.com>
2023-07-08 18:30:09 +0900

    til

--- Algorithm/2023/점프점프.py
+++ Algorithm/2023/점프점프.py
@@ -1,6 +1,23 @@
-x,y,p1,p2 = map(int,input().split())
-runx = set(range(p1, 10**6, x))
-runy = set(range(p2, 10**6, y))
+from collections import deque
+n = int(input())
+lst = list(map(int, input().split()))
+s = int(input())
+visited = [0]*n
+q = deque()
+q.append(s-1)
+visited[s-1] = 1
+while q:
+    now = q.popleft()
+    if now-lst[now] >= 0 and not visited[now-lst[now]]:
+        visited[now-lst[now]] = 1
+        q.append((now-lst[now]))
+    if now+lst[now] < n and not visited[now+lst[now]]:
+        visited[now+lst[now]] = 1
+        q.append((now+lst[now]))
+ans = 0
+for i in range(n):
+    if visited[i]:
+        ans += 1
+print(ans)
 
-R = runx&runy
-print(min(R) if R else -1)
+
