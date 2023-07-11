8a842751efafd09dd37b14ce861ced1d984184fd
HongJongKyu <kongi7064@gmail.com>
2023-07-11 21:05:07 +0900

    til

---
+++ react/root/day3/client.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom/client";
+import NumberBaseBall from "./NumberBaseball";
+
+ReactDom.createRoot(document.querySelector("#root")).render(<NumberBaseBall />);
