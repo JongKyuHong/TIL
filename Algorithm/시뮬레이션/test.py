34311f8f616990f638adc0068f7ae66f3221c10c
HongJongKyu <kongi7064@gmail.com>
2023-11-09 19:22:56 +0900

    시뮬레이션 연습문제

--- Algorithm/시뮬레이션/test.py
+++ Algorithm/시뮬레이션/test.py
@@ -1,77 +1,32 @@
 import sys 
 input = sys.stdin.readline
-dr = [0, 1, 0, -1]
-dc = [-1, 0, 1, 0]
-dir = [[[0, -2], [1, -1], [-1, -1], [-2, 0], [2, 0], [-1, 0], [1, 0], [-1, 1], [1, 1], [0, -1]],
-       [[2, 0], [1, -1], [1, 1], [0, -2], [0, 2], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, 0]],
-       [[0, 2], [1, 1], [-1, 1], [-2, 0], [2, 0], [-1, 0], [1, 0], [-1, -1], [1, -1], [0, 1]],
-       [[-2, 0], [-1, -1], [-1, 1], [0, -2], [0, 2], [0, -1], [0, 1], [1, -1], [1, 1], [-1, 0]]]
-       
-def rotate(r, c):
-    global cnt
-    while 1:
-        for i in range(2):
-            for _ in range(cnt):
-                nr, nc = dr[i]+r, dc[i]+c
-                spread(nr, nc, i)
-                lst[r][c] = 0
-                r, c = nr, nc
-                if r == 0 and c == 0:
-                    return
-        cnt += 1
-        for i in range(2, 4):
-            for _ in range(cnt):
-                nr, nc = dr[i]+r, dc[i]+c
-                spread(nr, nc, i)
-                r, c = nr, nc
-        cnt += 1
+dr = [0, -1, 0, 1]
+dc = [1, 0, -1, 0]
 
-def spread(r, c, d):
-    global answer
-    sand = lst[r][c]
-    remain = sand
-    for i in range(10):
-        x, y = dir[d][i]
-        if 0 <= x + r < N and 0 <= y + c < N:
-            if i == 0:
-                lst[x+r][y+c] += int(sand*0.05)
-                remain -= int(sand*0.05)
-            elif i == 1 or i == 2:
-                lst[x+r][y+c] += int(sand*0.1)
-                remain -= int(sand*0.1)
-            elif i == 3 or i == 4:
-                lst[x+r][y+c] += int(sand*0.02)
-                remain -= int(sand*0.02)
-            elif i == 5 or i == 6:
-                lst[x+r][y+c] += int(sand*0.07)
-                remain -= int(sand*0.07)
-            elif i == 7 or i == 8:
-                lst[x+r][y+c] += int(sand*0.01)
-                remain -= int(sand*0.01)
-            else:
-                lst[x+r][y+c] += int(remain)
-        else: # 밖으로 나간거?
-            if i == 0:
-                answer += int(sand*0.05)
-                remain -= int(sand*0.05)
-            elif i == 1 or i == 2:
-                answer += int(sand*0.1)
-                remain -= int(sand*0.1)
-            elif i == 3 or i == 4:
-                answer += int(sand*0.02)
-                remain -= int(sand*0.02)
-            elif i == 5 or i == 6:
-                answer += int(sand*0.07)
-                remain -= int(sand*0.07)
-            elif i == 7 or i == 8:
-                answer += int(sand*0.01)
-                remain -= int(sand*0.01)
-            else:
-                answer += int(remain)
-
-cnt = 1
-answer = 0
 N = int(input())
 lst = [list(map(int, input().split())) for _ in range(N)]
-rotate(N//2, N//2)
+visited = set()
+
+for x, y, d, g in lst:
+    x, y = y, x
+    coordinate = [d]
+    for i in range(g):
+        cor = coordinate[::-1]
+        for gg in cor:
+            gg = (gg + 1)%4
+            coordinate.append(gg)
+    #print(coordinate)
+    #visit = set()
+    visited.add((x, y))
+    #visit.add((x, y))
+    for gg in coordinate:
+        nr, nc = x+dr[gg], y+dc[gg]
+        visited.add((nr, nc))
+        #visit.add((nr, nc))
+        x, y = nr, nc
+answer = 0
+for i in range(1, 101):
+    for j in range(1, 101):
+        if (i, j) in visited and (i-1, j) in visited and (i, j-1) in visited and (i-1, j-1) in visited:
+            answer += 1
 print(answer)
